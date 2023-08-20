a883a1fbda4cf1e3bff18e3cfb637ad719344978
Akintunde Jegede <akintunde.jegede@primarybid.com>
2023-08-20 06:17:00 +0100

    updated linting

---
+++ src/create-schema/get-schema-creator.ts
@@ -0,0 +1,23 @@
+import { Config, KeyGroupSchemaCreator, RowedSchemaCreator } from "../types";
+import { getKeyGroupSchemaCreator } from "./create-key-group-schema";
+import { getRowedSchemaCreator } from "./create-rowed-schema";
+
+export function getSchemaCreator(config: Config, options: { type: "rowed" }): RowedSchemaCreator;
+export function getSchemaCreator(config: Config, options: { type: "key-group" }): KeyGroupSchemaCreator;
+
+export function getSchemaCreator(
+  config: Config,
+  options: { type: "rowed" | "key-group" },
+): RowedSchemaCreator | KeyGroupSchemaCreator {
+  const { type = "rowed" } = options;
+
+  if (type === "rowed") {
+    return getRowedSchemaCreator(config);
+  }
+
+  if (type === "key-group") {
+    return getKeyGroupSchemaCreator(config);
+  }
+
+  throw new Error("Invalid type");
+}
--- src/create-schema/index.ts
+++ src/create-schema/index.ts
@@ -1,23 +1,3 @@
-import { Config, KeyGroupSchemaCreator, RowedSchemaCreator } from '../types';
-import { getKeyGroupSchemaCreator } from './create-key-group-schema';
-import { getRowedSchemaCreator } from './create-rowed-schema';
-
-export function getSchemaCreator(config: Config, options: { type: 'rowed' }): RowedSchemaCreator;
-export function getSchemaCreator(config: Config, options: { type: 'key-group' }): KeyGroupSchemaCreator;
-
-export function getSchemaCreator(
-  config: Config,
-  options: { type: 'rowed' | 'key-group' },
-): RowedSchemaCreator | KeyGroupSchemaCreator {
-  const { type = 'rowed' } = options;
-
-  if (type === 'rowed') {
-    return getRowedSchemaCreator(config);
-  }
-
-  if (type === 'key-group') {
-    return getKeyGroupSchemaCreator(config);
-  }
-
-  throw new Error('Invalid type');
-}
+export * from "./create-rowed-schema";
+export * from "./create-key-group-schema";
+export * from "./get-schema-creator";
